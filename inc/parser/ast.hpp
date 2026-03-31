6123a688deddad5f94adc7d8e7f5b3e726024169
treltalex4 <shelalex06@gmail.com>
2026-03-31 19:38:15 +0500

    update AST node structures

--- inc/parser/ast.hpp
+++ inc/parser/ast.hpp
@@ -29,7 +29,6 @@ struct FieldAccess;
 struct CastExpr;
 struct ArrayLiteral;
 struct StructLiteral;
-struct GroupedExpr;
 
 // типы
 struct SimpleType;
@@ -93,7 +92,7 @@ using ExprNode =
                  std::unique_ptr<UnaryExpr>, std::unique_ptr<CallExpr>,
                  std::unique_ptr<IndexExpr>, std::unique_ptr<FieldAccess>,
                  std::unique_ptr<CastExpr>, std::unique_ptr<ArrayLiteral>,
-                 std::unique_ptr<StructLiteral>, std::unique_ptr<GroupedExpr>>;
+                 std::unique_ptr<StructLiteral>>;
 
 struct Expr {
     ExprNode node;
@@ -130,5 +129,138 @@ using DeclNode =
 struct Decl {
     DeclNode node;
     NodeLocation loc;
+};
+
+// определение сложных (рекурсивных) структур
+// выражения
+struct BinaryExpr {
+    TokenType op;
+    Expr left;
+    Expr right;
+};
+
+struct UnaryExpr {
+    TokenType op;
+    Expr operand;
+};
+
+struct CallExpr {
+    Identifier name;
+    std::vector<Expr> args;
+};
+
+struct IndexExpr {
+    Expr object;
+    Expr index;
+};
+
+struct FieldAccess {
+    Expr object;
+    std::string field;
+};
+
+struct CastExpr {
+    TypeExpr target_type;
+    Expr value;
+};
+
+struct ArrayLiteral {
+    std::vector<Expr> elements;
+};
+
+struct FieldInit {
+    std::string name;
+    Expr value;
+    NodeLocation loc;
+};
+
+struct StructLiteral {
+    Identifier name;
+    std::vector<FieldInit> fields;
+};
+
+
+
+// структуры
+struct ArrayType {
+    std::string size;
+    TypeExpr element;
+};
+
+// инструкции
+struct Param {
+    std::string name;
+    TypeExpr type;
+    NodeLocation loc;
+};
+
+struct Block {
+    std::vector<Stmt> statements;
+};
+
+struct LetStmt {
+    bool is_mutable;
+    std::string name;
+    TypeExpr type;
+    Expr init;
+};
+
+struct AssignStmt {
+    Expr target;  // lvalue: Identifier, IndexExpr или FieldAccess
+    Expr value;
+};
+
+struct ReturnStmt {
+    std::optional<Expr> value;  // void-функции: return; без значения
+};
+
+struct IfStmt {
+    Expr condition;
+    Block then_block;
+    // unique_ptr: else необязателен, а если есть — Block или ещё один IfStmt
+    std::optional<std::unique_ptr<Stmt>> else_branch;
+};
+
+struct WhileStmt {
+    Expr condition;
+    Block body;
+};
+
+struct ExprStmt {
+    Expr expression;
+};
+
+// объявления верхнего уровня
+struct FunctionDecl {
+    std::string name;
+    std::vector<Param> params;
+    TypeExpr return_type;
+    Block body;
+};
+
+struct StructField {
+    std::string name;
+    TypeExpr type;
+    NodeLocation loc;
+};
+
+struct StructDecl {
+    std::string name;
+    std::vector<StructField> fields;
+};
+
+struct NamespaceDecl {
+    std::string name;
+    std::vector<Decl> declarations;
+};
+
+struct TypeAliasDecl {
+    std::string name;
+    TypeExpr type;
+};
+
+// корень
+struct Program {
+    std::vector<Decl> declarations;
 };
 }  // namespace Parser
